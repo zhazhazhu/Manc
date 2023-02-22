435e2f5ee2b22917ab3d9f83f7a5edbadb480c21
zhazhazhu <fangyuan.leslie@qq.com>
2023-02-22 10:31:44 +0800

    [test] feat: button

--- packages/core/components/button/__test__/button.test.tsx
+++ packages/core/components/button/__test__/button.test.tsx
@@ -11,4 +11,36 @@ describe('Button.vue', () => {
 
     expect(classes).toContain('mc-button--primary')
   })
+
+  it('type', () => {
+    const wrapper = mount(() => <McButton type='danger'></McButton>)
+
+    const classes = wrapper.classes()
+
+    expect(classes).toContain('mc-button--danger')
+  })
+
+  it('link', () => {
+    const wrapper = mount(() => <McButton type='primary' link></McButton>)
+
+    const classes = wrapper.classes()
+
+    expect(classes).toContain('is-link')
+  })
+
+  it('disabled', () => {
+    const wrapper = mount(() => <McButton type='primary' disabled></McButton>)
+
+    const classes = wrapper.classes()
+
+    expect(classes).toContain('is-disabled')
+  })
+
+  it('plain', () => {
+    const wrapper = mount(() => <McButton type='primary' plain></McButton>)
+
+    const classes = wrapper.classes()
+
+    expect(classes).toContain('is-plain')
+  })
 })
